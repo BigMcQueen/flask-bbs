0fc9ab12f39dba4bdbea3a0211a4043b4e41880c
BigMcQueen <mmmrym10@gmail.com>
2022-12-30 11:19:07 +0900

    flask_sqlalchemyでdbを追加

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from pytz import timezone
 
 # create the extension
 db = SQLAlchemy()
@@ -12,8 +14,10 @@ db.init_app(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone('Asia/Tokyo')))
     username = db.Column(db.String(20))
     email = db.Column(db.String)
+    write = db.Column(db.String(200))
 
 @app.route('/')
 def index():
