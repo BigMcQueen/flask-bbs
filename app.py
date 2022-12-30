ac9e1497f33a2be2e7db1d952548cd65d18a3d28
BigMcQueen <mmmrym10@gmail.com>
2022-12-30 11:07:55 +0900

    base.htmlを追加

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+# create the extension
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    email = db.Column(db.String)
 
 @app.route('/')
 def index():
